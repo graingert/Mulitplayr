09ee4a8b49fa1b07316c54404cd2a55692d8f3ed
Daniel Stordy <daniel.stordy@googlemail.com>
2011-12-08 23:32:20 +0000

    Added urls to list and start games.

--- webapp/main.py
+++ webapp/main.py
@@ -3,5 +3,6 @@ import webapp2
 app = webapp2.WSGIApplication([
     ('/', 'handler.test.MainPage'),
     ('/newgame', 'simplegame.handlers.NewGameHandler'),
-    ('/listgame', 'simplegame.handlers.ListGameHandler')
+    ('/listgame', 'simplegame.handlers.ListGameHandler'),
+    ('/startgame', 'simplegame.handlers.StartGameHandler')
 ], debug=True)

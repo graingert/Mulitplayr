79e9bebb73446b083b42e87dc98e39473b2a9f3a
Daniel Stordy <daniel.stordy@googlemail.com>
2011-12-08 23:31:22 +0000

    Added a start game method to the simple game instance implementation.

--- webapp/simplegame/models.py
+++ webapp/simplegame/models.py
@@ -9,3 +9,12 @@ class SimpleGameState(BaseGameState):
 
 class SimpleGameInstance(BaseGameInstance):
     current_state = db.ReferenceProperty(SimpleGameState)
+    def start_game(self):
+        current_state = SimpleGameState()
+        current_state.current_number = 1
+        current_state.next_participant = self.participants[0]
+        current_state.put()
+
+        self.current_state = current_state
+        self.state = "playing"
+        self.put()

9e8e8428d343100657984542ef5d64c79d35f9b6
Daniel Stordy <daniel.stordy@googlemail.com>
2011-12-14 15:19:16 +0000

    Added a base action class and improved abstraction of the base game.

--- webapp/basegame/models.py
+++ webapp/basegame/models.py
@@ -4,10 +4,31 @@ from google.appengine.ext import db
 from google.appengine.ext.db import polymodel
 from google.appengine.api import users
 
+class BaseGameState(polymodel.PolyModel):
+    current_participant = db.UserProperty()
+    last_sequence_number = db.IntegerProperty(required=True, default=0)
+
+    def get_actions_since(self,seq_num):
+        return self.basegameaction_set.filter('sequence_number >', seq_num)
+
+    def add_action(self,action):
+        action.game_state = self
+        action.sequence_number = last_sequence_number
+        last_sequence_number += 1
+        action.participant = current_participant
+        action.put()
+        self.put()
+
+class BaseGameAction(polymodel.PolyModel):
+    game_state = db.ReferenceProperty(BaseGameState)
+    sequence_number = db.IntegerProperty(required=True)
+    participant = db.UserProperty()
+
 class BaseGameInstance(polymodel.PolyModel):
     state = db.StringProperty(required=True, choices=set(["open", "playing", "finished"]))
     created = db.DateProperty(required=True)
     participants = db.ListProperty(users.User)
+    current_state = db.ReferenceProperty(BaseGameState)
 
     def start_game(self):
         # Check if the game can be started
@@ -15,7 +36,10 @@ class BaseGameInstance(polymodel.PolyModel):
             return False
 
         # Set the current to the initial state
-        self.current_state = self.new_initial_state()
+        new_state = self.new_initial_state()
+        new_state.current_participant = self.participants[0]
+        new_state.put()
+        self.current_state = new_state
         self.state = "playing"
         self.put()
         return self.current_state
@@ -32,6 +56,3 @@ class BaseGameInstance(polymodel.PolyModel):
         self.participants.append(user)
         self.put()
         return True
-
-class BaseGameState(db.Model):
-    next_participant = db.UserProperty()
--- webapp/simplegame/models.py
+++ webapp/simplegame/models.py
@@ -9,10 +9,10 @@ class SimpleGameState(BaseGameState):
     current_number = db.IntegerProperty()
 
 class SimpleGameInstance(BaseGameInstance):
-    current_state = db.ReferenceProperty(SimpleGameState)
     def new_initial_state(self):
         current_state = SimpleGameState()
         current_state.current_number = 1
-        current_state.next_participant = self.participants[0]
-        current_state.put()
         return current_state
+
+class SimpleGameAction(BaseGameInstance):
+    pass

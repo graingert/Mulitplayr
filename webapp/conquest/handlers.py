8c325cd91c08147cd2536f8187ecb799af7f590e
Daniel Stordy <daniel.stordy@googlemail.com>
2012-01-04 12:42:26 +0000

    Added start of conquest game type.

---
+++ webapp/conquest/handlers.py
@@ -0,0 +1,50 @@
+import webapp2
+import datetime
+import json
+
+from google.appengine.api import users
+from google.appengine.ext.db import Key
+
+from base import BaseHandler, JSONEncoderGAE
+from models import *
+from basegame.handlers import *
+
+class NewGameHandler(BaseHandler):
+    def get(self):
+        user = users.get_current_user()
+
+        if not user:
+            self.redirect(users.create_login_url(self.request.uri))
+            return
+
+        now = datetime.datetime.now().date();
+        instance = SimpleGameInstance(state = 'open',
+                                      created = now,
+                                      players = [user])
+        instance.put()
+        return webapp2.redirect_to('lobby')
+
+class SimpleGameInfoHandler(GameInfoHandler):
+    @login_required
+    def get_page(self, game_instance):
+        self.prepare_context(game_instance)
+
+        self.render_response('game_info.html')
+
+class SimpleGamePlayHandler(GamePlayHandler):
+
+    def __init__(self, request, response):
+        GamePlayHandler.__init__(self, request, response)
+
+        self.postHandlers['guess'] = self.guess_action
+
+    def get_page(self, game_instance):
+        game_state = game_instance.current_state
+
+        self.prepare_context(game_instance)
+        self.render_response('play_simple.html')
+
+    def guess_action(self, game_instance):
+        game_state = game_instance.current_state
+        new_action = game_state.guess_action(int(self.request.get('guess')))
+        self.post_action(game_state, new_action)
--- webapp/main.py
+++ webapp/main.py
@@ -3,16 +3,19 @@ import webapp2
 config = {
     'game_models' :
     [
-        'simplegame.models'
+        'simplegame.models',
+        'conquest.models'
     ]
 }
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', 'handler.test.MainPage'),
     webapp2.Route('/lobby/', 'basegame.handlers.LobbyHandler','lobby'),
-    webapp2.Route('/newgame/', 'simplegame.handlers.NewGameHandler', 'newgame'),
+    webapp2.Route('/newgame/', 'conquest.handlers.NewGameHandler', 'newgame'),
     webapp2.Route('/game/<game_id>/', 'basegame.handlers.GameInfoHandler', 'gameinfo'),
     webapp2.Route('/game/<game_id>/play/', 'basegame.handlers.GamePlayHandler', 'gameplay'),
     webapp2.Route('/game/simple/<game_id>/', 'simplegame.handlers.SimpleGameInfoHandler', 'simplegameinfo'),
     webapp2.Route('/game/simple/<game_id>/play/', 'simplegame.handlers.SimpleGamePlayHandler', 'simplegameplay'),
+    webapp2.Route('/game/conquest/<game_id>/', 'simplegame.handlers.SimpleGameInfoHandler', 'conquestgameinfo'),
+    webapp2.Route('/game/conquest/<game_id>/play/', 'simplegame.handlers.SimpleGamePlayHandler', 'conquestgameplay'),
 ], debug=True, config=config)

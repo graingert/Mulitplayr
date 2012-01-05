1af6215964bcb89ae1eb4b4b317148f6fd8a335f
Nicholas Hollett <niax@niax.co.uk>
2012-01-05 14:04:37 +0000

    Fixed index page spaffing.
    Now uses proper message & stuffs

--- webapp/handler/test.py
+++ webapp/handler/test.py
@@ -3,5 +3,5 @@ from base import BaseHandler
 
 class MainPage(BaseHandler):
     def get(self):
-        context = {'message':'This is a test templated message'}
-        self.render_response('index.html', **context)
+        self.context['message'] = 'This is a test templated message'
+        self.render_response('index.html')
